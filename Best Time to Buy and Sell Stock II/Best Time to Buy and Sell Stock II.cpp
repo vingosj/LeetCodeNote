33b9c0439edb782f1b824c0d591233395e4c908c
vingo Chen <vingosheng@gmail.com>
2015-06-14 14:03:15 -0700

    Best Time to Buy and Sell Stock II

---
+++ Best Time to Buy and Sell Stock II/Best Time to Buy and Sell Stock II.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+
+//   2015-6-14
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if(n == 0) return 0;
+        
+        int sum = 0;
+        for(int i = 1; i < n; i++){
+            if(prices[i] > prices[i-1]) sum += (prices[i] - prices[i-1]);
+        }
+        
+        return sum;
+    }
+};
