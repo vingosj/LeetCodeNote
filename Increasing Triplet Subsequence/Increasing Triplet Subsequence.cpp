012c93ddfe060a70f30a0cabeb23232ef70bd830
vingo Chen <vingosheng@gmail.com>
2016-03-23 11:20:52 +0800

    Increasing Triplet Subsequence

---
+++ Increasing Triplet Subsequence/Increasing Triplet Subsequence.cpp
@@ -0,0 +1,16 @@
+//   2016-3-23
+
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int n = nums.size(), a = INT_MAX, b = INT_MAX;
+        
+        for(int i = 0; i < n; i++)
+        {
+            if(nums[i] <= a) a = nums[i];
+            else if(nums[i] <= b) b = nums[i];
+            else if(nums[i] > b) return true;
+        }
+        return false;
+    }
+};
